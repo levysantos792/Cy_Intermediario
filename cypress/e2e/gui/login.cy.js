eff5bf30e9dc11d41a4de23094ed98d35f282e6b
Levisantos <levi.santos@melhorenvio.com>
2025-03-30 19:44:53 -0300

    update

---
+++ cypress/e2e/gui/login.cy.js
@@ -0,0 +1,9 @@
+describe('Login', () => {
+  it('successfully', () => {
+    const user = Cypress.env('user_name')
+    const password = Cypress.env('user_password')
+    const options= {cacheAcrossSpecs: false}
+    cy.login(user, password, options)
+    cy.get('.qa-user-avatar').should('be.visible')
+  })
+})
--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,12 +1,35 @@
 Cypress.Commands.add('login', (
-    user= Cypress.env('user_name'),
-    password= Cypress.env('user_password')
-) => {
+    user = Cypress.env('user_name'),
+    password = Cypress.env('user_password'),
+    { cacheSession = true } = {},) => {
     const login = () => {
-        cy.visit('/user/sing_in')
-        cy.get()
-        cy.get()
-        cy.get()
-        cy.get()
+      cy.visit('/users/sign_in')
+  
+      cy.get("[data-qa-selector='login_field']").type(user)
+      cy.get("[data-qa-selector='password_field']").type(password, { log: false })
+      cy.get("[data-qa-selector='sign_in_button']").click()
     }
+    const options = {
+        cacheAcrossSpecs: true,
+      }
+    if (cacheSession) {cy.session(user, login, options)}
+    else {
+        login()}
+  })
+
+Cypress.Commands.add('logout', () => {
+    cy.get('[data-qa-selector="user_menu"]').click()
+    cy.get('[data-qa-selector="sign_out_link"]').click()
+
 })
+
+Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('http://localhost/projects/new')
+
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('#project_initialize_with_readme').check()
+    cy.get('input[name="commit"][data-track-label="blank_project"]').click();
+
+})
+  
